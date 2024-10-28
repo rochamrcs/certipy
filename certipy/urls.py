bc5ee11812e7ae0b2cd8898e82d7b1b3acaa9643
rochamrcs <mavinicius95@gmail.com>
2024-10-28 08:57:35 -0300

    Atualizando projeto

--- certipy/urls.py
+++ certipy/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from core import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Participante(models.Model):
     order = models.CharField(max_length=100)
     nome = models.CharField(max_length=100)
