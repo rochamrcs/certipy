178eb7776f2054fb634aae89cc05c47018fe80e4
rochamrcs <mavinicius95@gmail.com>
2024-10-28 09:11:20 -0300

    Ajustes na configuração do projeto

--- certipy/urls.py
+++ certipy/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import urls
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
